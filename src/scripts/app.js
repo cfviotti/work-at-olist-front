861097955e1e64f3463c34f45b063f4a0c2f129f
Caio <caio@viotti.me>
2019-10-01 22:17:43 -0300

    Setting up base project files
    This is adapted from https://github.com/tr1s/tris-webpack-boilerplate
    I Removed some stuff I considered unnecessary from the boilerplate for this
    project, and modified a few stuff, like adding live-reload and reorganizing the file
    structure to my liking.

---
+++ src/scripts/app.js
@@ -0,0 +1,10 @@
+require('offline-plugin/runtime').install();
+
+// Importing markup
+import '../index.html';
+
+// Importing styles
+import '../styles/app.scss';
+
+// Importing our scripts
+// New JS goes here...
