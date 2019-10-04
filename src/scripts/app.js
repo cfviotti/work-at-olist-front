ee406bba8561971d7127b2355a3e07adee2a363e
Caio <caio@viotti.me>
2019-10-04 03:03:21 -0300

    Fix empty input styles

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,7 +6,10 @@ import '../index.html';
 // Importing styles
 import '../styles/app.scss';
 
-// Importing our scripts
+// Importing our main scripts
 import './validate-password.js';
 import './validate-full-name.js';
 import './validate-email.js';
+
+// Utils scripts
+import './utils/empty-inputs.js';
