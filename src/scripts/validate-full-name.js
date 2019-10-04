3ef9c9e85a494fc51a70a5159ba81048f4fd18ea
Caio <caio@viotti.me>
2019-10-04 03:08:03 -0300

    Deprecate some JS validation in favor of native HTML5 validation

--- src/scripts/validate-full-name.js
+++ src/scripts/validate-full-name.js
@@ -10,12 +10,5 @@ let isValidFullName = (nameInputValue) => {
   // Testing for at least two words, not requiring capital letters
   const fullNameRegex = /(\w.+\s).+/i
 
-  if (fullNameRegex.test(nameInputValue)) {
-    nameInputElement.classList.add('-valid')
-    return true
-
-  } else {
-    nameInputElement.classList.remove('-valid')
-    return false
-  }
+  return fullNameRegex.test(nameInputValue)
 }
